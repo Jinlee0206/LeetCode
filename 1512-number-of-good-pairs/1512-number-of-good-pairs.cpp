ca792ef27447645ec41276ac31cdca1a5dc6167e
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-03-09 11:27:21 +0900

    Time: 5 ms (28.81%), Space: 7.4 MB (15.85%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -3,14 +3,15 @@ static const int _=[](){ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nu
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int gp = 0;
-        for(int i = 0; i< nums.size() - 1; i++)
+        unordered_map<int,int> um;
+        int cnt= 0;
+        
+        for(auto i : nums) um[i]++;
+        
+        for(auto idx : um)
         {
-            for(int j= i+1; j< nums.size(); j++)
-            {
-                if(nums[i]== nums[j]) gp++;
-            }
+            if(idx.second > 1) cnt += idx.second * (idx.second-1) / 2; 
         }
-        return gp;
+        return cnt;
     }
 };
